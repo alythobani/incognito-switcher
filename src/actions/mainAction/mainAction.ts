0e7237a5a95b12e30925bed1ad688b50c63afebf
Aly Thobani <athobani9@gmail.com>
2024-03-19 13:03:54 -0700

    Small refactor

--- src/actions/mainAction/mainAction.ts
+++ src/actions/mainAction/mainAction.ts
@@ -3,5 +3,5 @@ import { openExtensionSettings } from "./openExtensionSettings";
 
 export const onMainAction = async (tab: chrome.tabs.Tab): Promise<void> => {
   log("onMainAction", tab, tab.url, tab.incognito);
-  await openExtensionSettings(tab);
+  await openExtensionSettings();
 };
--- src/actions/mainAction/openExtensionSettings.ts
+++ src/actions/mainAction/openExtensionSettings.ts
@@ -1,6 +1,6 @@
 import { getExtensionSettingsURL } from "../../utils/chromeUtils";
 import { createNewTab } from "../tabActions/createNewTab";
 
-export const openExtensionSettings = async (tab: chrome.tabs.Tab): Promise<void> => {
+export const openExtensionSettings = async (): Promise<void> => {
   await createNewTab({ url: getExtensionSettingsURL(), mode: "normal" });
 };
