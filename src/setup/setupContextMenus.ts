f9ff524be32c23f30bfd8f3bcae0b2af81f5e7a9
Aly Thobani <athobani9@gmail.com>
2024-03-19 23:43:07 -0700

    Remove unnecessary call

--- src/setup/setupContextMenus.ts
+++ src/setup/setupContextMenus.ts
@@ -1,10 +1,8 @@
 import { onContextMenuItemClicked } from "../actions/contextMenus/contextMenus";
-import { createContextMenuItems } from "../providers/contextMenus";
 import { convertToCallback } from "../utils/utils";
 
 /* Exports */
 
 export function setupContextMenus(): void {
-  chrome.runtime.onInstalled.addListener(convertToCallback(createContextMenuItems));
   chrome.contextMenus.onClicked.addListener(convertToCallback(onContextMenuItemClicked));
 }
